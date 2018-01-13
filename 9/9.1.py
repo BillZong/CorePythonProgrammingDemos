27b2ffbc7a42aeb6b7cb064b67378610bcedc15b
BillZong <billzong2012@gmail.com>
2018-01-13 13:46:06 +0800

    [Modify]: Another testing way for # comments filtering, not done yet.

--- 9/9.1.py
+++ 9/9.1.py
@@ -8,18 +8,33 @@ import re
 
 def show_all_lines(file_path):
     """Show out all lines of a file, ignore comment lines."""
-    def is_pass_line(str_line):
+    def str_without_comment_in_line(str_line):
+        stip_line = str_line.strip()
+        if stip_line == '':
+            return '\n'
+        elif stip_line == '\n':
+            return stip_line
         # 是否是可以忽略的行
         # 可忽略行的正则表达式列表
-        regular_expressions = ["""/'.*#.*/'""", """/\".*#.*/\"
-""", """/'/'/'.*#.*/'/'/'""", """/\"/\"/\".*#.*/\"/\"/\""""]
-        for one in regular_expressions:
-            zz = re.compile(one)
-            if re.search(zz, str_line) is None:
-                continue
-            else:
-                return True  # 有匹配 则忽略
-        return False
+        reg = "[^\'\"]*.*#.*[^\'\"]*"
+        z = re.compile(reg)
+        match = re.search(z, str_line)
+        if match:
+            m = re.match(z, str_line)
+            return m.group(0) + '\n'
+        else:
+            return str_line
+#         regular_expressions = ["""\'.*#.*\'""", """\".*#.*\"
+# """, """\'\'\'.*#.*\'\'\'""", """\"\"\".*#.*\"\"\""""]
+#         for one in regular_expressions:
+#             # try:
+#             zz = re.compile(one)
+#             # except re.SyntaxError(" error"):
+#             if re.search(zz, str_line) is None:
+#                 continue
+#             else:
+#                 return True  # 有匹配 则忽略
+#         return False
 
     try:
         f = open(file_path)
@@ -27,12 +42,14 @@ def show_all_lines(file_path):
         return None
 
     for line in f.readlines():
-        index = line.find('#')  # 获取带注释句'#'的位置索引
-        if index == -1 or is_pass_line(line):
-            if line.strip() != '':  # 排除纯空的行
-                print line,
-        elif index != 0:
-            if line.strip() != '':  # 排除纯空的行
-                print line[:index] + '\n'
+        # print line.strip() + '\n',
+        print str_without_comment_in_line(line),
+        # index = line.find('#')  # 获取带注释句'#'的位置索引
+        # if index == -1 or is_pass_line(line):
+        #     if line.strip() != '':  # 排除纯空的行
+        #         print line,
+        # elif index != 0:
+        #     if line.strip() != '':  # 排除纯空的行
+        #         print line[:index] + '\n'
 
 show_all_lines('9.1.py')
